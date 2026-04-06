5f8f1c2ad73d127d1e0dec093ed671f25018d5ae
Ikram Bekkaoui <ikkram5222@gmail.com>
2026-04-06 17:04:21 +0200

    feat: added CORS middleware and dummy fMRI data endpoint to FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
 
 #initializing Mindora
 
@@ -7,9 +9,26 @@ app = FastAPI(
     description="Backend for TRIBE V2 neural response prediction",
     version="1.0.0",
 )
+###CORS SETUP###
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Mindora API!"}
 @app.get("/health")
-async def health():
+async def health_check():
     return {"status": "healthy", "model_loaded": False}
+
+###DUMMY ENDPOINTS FOR TESTING PURPOSES###
+@app.get("/api/predict-dummy")
+async def get_dummy_brain_data():
+    # Simulate TRIBE V2 fMRI output: an array of 100 activation values between 0.0 and 1.0 (List Comprehension)
+    activation_data = [round(random.uniform(0.0, 1.0), 3) for _ in range(100)]
+    return {"status": "success","model":"dummy-v1", "data": activation_data}
